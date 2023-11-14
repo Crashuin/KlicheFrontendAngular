13d3b2fcc64324614df44c36ba3ec00edf890487
victorChacon <victorch707@gmail.com>
2023-11-14 11:31:19 -0500

    Cambios del footer frontend cliente

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,12 +9,14 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     SidenavComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
@@ -27,7 +29,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   exports: [
     SidenavComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
